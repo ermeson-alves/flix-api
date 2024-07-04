2cffe624baa613d81faf00a4e3bda899e3e8e1d2
ermeson <ermeson3alves@gmail.com>
2024-07-04 18:28:33 -0300

    Adicionando rotas de verificação e atualização de token

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 * Autenticação != Permissão;
 * A partir do momento que protegemos os endpoints as requisições passam a ser testadas via Postman, para informar um Baerer;
 * Existem varias classes de permissão que permite garantir diferentes niveis de acesso por tipo/grupo de usuário;
+* o refresh token serve para atualizar o token sem precisar autenticar novamente;
--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+ )
+
 
 urlpatterns = [
     path('authentication/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # rota para obtenção de tokens com JWT
+    path('authentication/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # rota para atualizar o token
+    path('authentication/token/verify/', TokenVerifyView.as_view(), name='token_verify'), # rota para verificar o token
 ]
